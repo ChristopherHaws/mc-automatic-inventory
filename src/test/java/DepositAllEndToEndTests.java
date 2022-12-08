8761112b86dd496f0844a0ef8b535aa705a4935a
Christopher Haws <christopher.haws@arvato-scs.com>
2022-12-08 01:10:14 -0800

    Add new test

--- src/test/java/DepositAllEndToEndTests.java
+++ src/test/java/DepositAllEndToEndTests.java
@@ -3,15 +3,21 @@ import be.seeseemelk.mockbukkit.ServerMock;
 import be.seeseemelk.mockbukkit.WorldMock;
 import be.seeseemelk.mockbukkit.block.state.ChestMock;
 import dev.chaws.automaticinventory.AutomaticInventory;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockDataMeta;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DepositAllEndToEndTests {
+	private static final Logger logger = LoggerFactory.getLogger(DepositAllEndToEndTests.class);
+
 	private ServerMock server;
 	private AutomaticInventory plugin;
 
@@ -43,12 +49,28 @@ public class DepositAllEndToEndTests {
 		Assertions.assertTrue(player.performCommand("depositall"));
 
 //		server.getScheduler().performTicks(1000L);
-		while (!server.getScheduler().getPendingTasks().isEmpty()) {
-			server.getScheduler().waitAsyncTasksFinished();
-		}
+//		while (server.getScheduler().getNumberOfQueuedAsyncTasks() != 0) {
+//			server.getScheduler().waitAsyncTasksFinished();
+//		}
+
+		server.getScheduler().waitAsyncTasksFinished();
+		server.getScheduler().performTicks(1000L);
 
 
 		Assertions.assertFalse(player.getInventory().contains(Material.STONE));
 		Assertions.assertTrue(chest.getInventory().contains(Material.STONE, 2));
+	}
+
+	@Test
+	void foo() {
+		Assertions.assertDoesNotThrow(() -> {
+			for (Material mat : Material.values()) {
+				var legacy = Bukkit.getUnsafe().fromLegacy(mat);
+
+				if (mat != legacy) {
+					logger.info(mat.toString() + ": " + legacy.toString());
+				}
+			}
+		});
 	}
 }
