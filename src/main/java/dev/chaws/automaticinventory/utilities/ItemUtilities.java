cbd84191c25982cd742a5a26fbcbdb993d1c8a6e
Christopher Haws <ChristopherHaws@users.noreply.github.com>
2024-07-14 22:06:16 -0700

    Retarget to 1.21

--- src/main/java/dev/chaws/automaticinventory/utilities/ItemUtilities.java
+++ src/main/java/dev/chaws/automaticinventory/utilities/ItemUtilities.java
@@ -1,6 +1,5 @@
 package dev.chaws.automaticinventory.utilities;
 
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 
@@ -25,47 +24,29 @@ public class ItemUtilities {
 		//differentiate potion types. Original credit to pugabyte: https://github.com/Pugabyte/AutomaticInventory/commit/01bbdbfa0ea1bc7dc397fc8a8ff625f3f22e1ed6
 		//Modified to use PotionType instead of PotionEffectType in signature
 		if (stack.getType().toString().toLowerCase().contains("potion")) {
-			var potionData = ((PotionMeta) stack.getItemMeta()).getBasePotionData();
-			signature += "." + potionData.getType();
-			if (potionData.isExtended()) {
-				signature += ".extended";
+			var itemMeta = stack.getItemMeta();
+			if (itemMeta == null) {
+				return signature;
 			}
-			if (potionData.isUpgraded()) {
-				signature += ".upgraded";
+
+			if (!(itemMeta instanceof PotionMeta potionMeta)) {
+				return signature;
+			}
+
+			var potionType = potionMeta.getBasePotionType();
+			if (potionType == null) {
+				return signature;
+			}
+
+			signature += "." + potionType;
+			if (potionType.isExtendable()) {
+				signature += ".extendable";
+			}
+			if (potionType.isUpgradeable()) {
+				signature += ".upgradable";
 			}
 		}
 
 		return signature;
-	}
-
-	public static boolean itemsAreSimilar(ItemStack a, ItemStack b) {
-		if (a.getType() == b.getType()) {
-			return
-				!a.containsEnchantment(Enchantment.LOOT_BONUS_BLOCKS) &&
-				!a.containsEnchantment(Enchantment.SILK_TOUCH) &&
-				!a.containsEnchantment(Enchantment.LOOT_BONUS_MOBS) &&
-				!a.containsEnchantment(Enchantment.ARROW_INFINITE);
-
-			//a will _not_ have itemMeta if it is a vanilla tool with no damage.
-//            if(a.hasItemMeta() != b.hasItemMeta()) return false;
-//
-//            //compare metadata
-//            if(a.hasItemMeta())
-//            {
-//                if(!b.hasItemMeta()) return false;
-//
-//                ItemMeta meta1 = a.getItemMeta();
-//                ItemMeta meta2 = b.getItemMeta();
-//
-//                //compare names
-//                if(meta1.hasDisplayName())
-//                {
-//                    if(!meta2.hasDisplayName()) return false;
-//                    return meta1.getDisplayName().equals(meta2.getDisplayName());
-//                }
-//            }
-		}
-
-		return false;
 	}
 }
